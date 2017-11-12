059ac0a09716930cf41cc28caa0180d872888775
TM <github@timsmanter.net>
2017-11-12 17:05:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+TM-P_ALTAR_API_SYM_PHP_PART
+===========================
+
+A Symfony project created on October 12, 2017, 10:37 pm.
---
+++ src/AppBundle/Controller/API/PostController.php
@@ -0,0 +1,52 @@
+<?php
+namespace AppBundle\Controller\API;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Post;
+use AppBundle\Entity\Channel;
+/**
+ * @Route("/api")
+ */
+class PostController extends Controller
+{
+  /**
+   * @Route("/posts", name="getPosts")
+   * @Method("GET")
+   */
+  public function getPostsAction(Request $request) {
+    $em = $this->getDoctrine()->getManager();
+    $posts = $em->getRepository(Post::class)->findAll();
+    $data = [];
+    $channel = $em->getRepository(Channel::class)->findAll()[0];
+    foreach ($posts as $post) {
+      $postData = [
+        "id" => $post->getId(),
+        "path" => $post->getPath(),
+        "createDate" => $post->getCreateDate(),
+        "publishDate" => $post->getPublishDate(),
+        "isVisible" => $post->getIsVisible(),
+        "author" => null,
+        "type" => [
+          "id" => 1,
+          "name" => "Image"
+        ],
+        "channels" => [ $channel ]
+      ];
+      $data[] = $postData;
+    }
+    return $this->json($data);
+  }
+
+  /**
+   * @Route("/posts", name="createPost")
+   * @Method("POST")
+   */
+  // public function createPostAction(Request $request) {
+  //   $em = $this->getDoctrine()->getManager();
+  //   $posts = $em->getRepository(Post::class)->findAll();
+  //   return $this->json($posts);
+  // }
+}
